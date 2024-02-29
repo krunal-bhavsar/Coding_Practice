6e00c563a74b2072f881118fa0f0590f3d4ed1a1
therealspark9 <sachhu98@gmail.com>
2024-02-29 14:02:40 +0530

    solved groupanagrams

--- spark9/groupanagrams.py
+++ spark9/groupanagrams.py
@@ -1,20 +1,18 @@
-a_dict=dict()
-j=1
-for i in range(len(strs)):
-    temp=''.join(sorted(strs[i]))
-    if temp not in a_dict:
+class Solution:
+    # def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    def groupAnagrams(self, strs):    
+        a_dict=dict()
         j=1
-        a_dict[temp]=j
-    else:
-        j+=1
-        a_dict[temp]=j
-a_list=[""]*len(a_dict)
-# print(a_list)
-for i in range(len(strs)):
-    temp=''.join(sorted(strs[i]))
-    if temp not in a_dict:
-        a_list[i]=strs[i]
-    else:
-        temp=a_list[i]
-        a_list[i]=[temp, strs[i]]
-print(a_list)
+        for i in range(len(strs)):
+            temp=''.join(sorted(strs[i]))
+            if temp not in a_dict:
+                j=1
+                a_dict[temp]=[strs[i]]
+            else:
+                j+=1
+                a_dict[temp].append(strs[i])
+        a_list=list(a_dict.values())
+        return a_list
+    
+strs=["eat","tea","tan","ate","nat","bat"]
+print(Solution().groupAnagrams(strs))
