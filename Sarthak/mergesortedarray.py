959c9f742dc5a7aaacadffe51268ee0b8c00fb1e
Hackdit <mishra.sarthak.5@gmail.com>
2024-03-01 21:50:59 -0500

    optimized code

--- Sarthak/mergesortedarray.py
+++ Sarthak/mergesortedarray.py
@@ -1,22 +1,12 @@
-nums1 = [1,2,3,0,0,0]
-m = 3
-nums2 = [2,5,6]
-n = 3
-
-def merge(self, nums1, m , nums2, n) -> None:
-        p=m-1
-        q=n-1
+class Solution:
+    nums1 = [1,2,3,0,0,0] 
+    m = 3
+    nums2 = [2,5,6] 
+    n = 3
+    def merge(nums1, m, nums2, n):
         z=0
-        i=m+n-1
         for element in range(z,n):
             nums1[m]= nums2[z]
-            p+=1
             z+=1
             m+=1
         nums1.sort()
-        print(nums1)
-
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        
