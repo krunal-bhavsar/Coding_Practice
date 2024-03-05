663c0f6c82159de9a0a3001322234f85f9eb655b
Hackdit <mishra.sarthak.5@gmail.com>
2024-03-04 22:28:21 -0500

    Added Remove Element

--- Sarthak/RemoveElement.py
+++ Sarthak/RemoveElement.py
@@ -8,3 +8,7 @@ class Solution:
                 nums[k]=nums[i]
                 k+=1
         return k
+    
+''' what i used here is basically a 2 pointer method,
+such that i pointer iterates over the array and the k pointer is used to swap elements of the array
+such is achieved with another loop within the iterration'''
