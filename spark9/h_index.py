2989ec5582d442f2c8e6be54f1e93e6aecc485d0
therealspark9 <sachhu98@gmail.com>
2024-03-06 01:17:49 +0530

    updated h_index

--- spark9/h_index.py
+++ spark9/h_index.py
@@ -1,8 +1,7 @@
 class Solution:
-    def hIndex(self, citations: list[int]) -> int:
+    def hIndex(self, citations: List[int]) -> int:
         citations.sort()
         n=len(citations)
-        count=0
         for i in citations:
             if i<n:
                 n-=1
